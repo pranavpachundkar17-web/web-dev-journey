8e975b17a996350126b3328d1149f90ef902992e
Pranav Pachundkar <pranavpachundkar17@gmail.com>
2026-03-31 11:10:07 +0530

    Create datatypes.js

---
+++ week-3-js/datatypes.js
@@ -0,0 +1,7 @@
+console.log(typeof "hello");
+console.log(typeof 42);
+console.log(typeof true);
+console.log(typeof null);      // remember the bug
+console.log(typeof undefined);
+console.log(typeof {});
+console.log(typeof []);
