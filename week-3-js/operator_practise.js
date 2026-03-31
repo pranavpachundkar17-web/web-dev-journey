ee6ff4aec78a9e05faf09a53e750045dd877366a
Pranav Pachundkar <pranavpachundkar17@gmail.com>
2026-03-31 11:14:01 +0530

    "week 3 - operators practice"

---
+++ week-3-js/operator_practise.js
@@ -0,0 +1,11 @@
+let x = 15;
+x += 10;
+console.log(x);      // 25
+
+console.log(10 % 3); // 1
+console.log(2 ** 8); // 256
+
+let n = 5;
+console.log(n++);    // 5
+console.log(n);      // 6
+console.log(++n);    // 7
