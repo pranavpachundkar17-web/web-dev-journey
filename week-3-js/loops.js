b38c19ea053aaacc437aeb0877d1caaf0191338f
Pranav Pachundkar <pranavpachundkar17@gmail.com>
2026-03-31 11:30:31 +0530

    "week 3 - loops practice"

---
+++ week-3-js/loops.js
@@ -0,0 +1,16 @@
+// 1. Print 1 to 10 using for loop
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+// 2. Print even numbers from 1 to 20
+for (let i = 2; i <= 20; i += 2) {
+    console.log(i);
+}
+
+// 3. while loop counting down from 5 to 1
+let count = 5;
+while (count >= 1) {
+    console.log(count);
+    count--;
+}
