2dea2998a2e3a69a9710e02a47fdba1bd13b4192
Pranav Pachundkar <pranavpachundkar17@gmail.com>
2026-03-31 11:18:09 +0530

    "week 3 - type conversion practice"

---
+++ week-3-js/typeconversion.js
@@ -0,0 +1,11 @@
+console.log(Number("42"));
+console.log(Number("hello"));
+console.log(Number(true));
+console.log(Number(null));
+
+console.log(String(100));
+console.log(String(null));
+
+console.log(Boolean(0));
+console.log(Boolean("false"));  // the trap
+console.log(Boolean([]));
